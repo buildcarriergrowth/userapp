ba50774fe812cda96fd5f094092f9d493017490e
anilkumar1334 <anilalapati.kumar682@gmail.com>
2019-05-25 08:46:50 +0530

    modified testcase

--- test/usertest.js
+++ test/usertest.js
@@ -5,7 +5,7 @@ let UserForm = require("../src/components/UserForm");
 
 describe("Userdetails component",function(){
     it("testig",function(){
-        
+        expect(true).to.be.ok;  
         expect(true).to.be.ok;
     })
 })
